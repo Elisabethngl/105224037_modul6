baf07c641d3bfb05b7de6ca3cd2090d6771672ca
Elisabeth Nainggolan <sabethngl742@gmail.com>
2026-04-14 09:26:39 +0700

    Benerin Main Java dan gabungin semua tugas

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,43 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Karyawan> listKaryawan = new ArrayList<>();
-
-        listKaryawan.add(new Karyawan("039", "Haruka", 2025, 6000000, 3.0));
-        listKaryawan.add(new Developer("001", "Silviana", 2022, 9000000, 4.0, "Senior", 20));
-        listKaryawan.add(new Developer("033", "Zakia", 2024, 7000000, 3.8, "Mid", 15));
-        listKaryawan.add(new Manajer("028", "Neila", 2019, 13000000, 4.8, "IT", 10));
-        listKaryawan.add(new Manajer("037", "Elisabeth", 2023, 10000000, 4.5, "HR", 5));
-
-        double totalAnggaran = 0;
-        double totalRating = 0;
-        Karyawan highestEarner = listKaryawan.get(0);
-
+     
         System.out.println();
-        for (Karyawan k : listKaryawan) {
-            double gaji = k.hitungGajiTotal(); 
+
+        ArrayList<Karyawan> daftarKaryawan = new ArrayList<>(); 
+
+       
+        daftarKaryawan.add(new Karyawan("039", "Haruka", 2025, 6000000, 3.0));
+        daftarKaryawan.add(new Developer("001", "Silviana", 2022, 9000000, 4.0, "Senior", 20));
+        daftarKaryawan.add(new Developer("033", "Zakia", 2024, 7000000, 3.8, "Mid", 15));
+        daftarKaryawan.add(new Manajer("028", "Neila", 2019, 13000000, 4.8, "IT", 10));
+        daftarKaryawan.add(new Manajer("037", "Elisabeth", 2023, 10000000, 4.5, "HR", 5));
+
+        double totalPengeluaranGaji = 0;
+        double akumulasiRating = 0;
+        Karyawan karyawanGajiTertinggi = daftarKaryawan.get(0);
+
+        System.out.println("=== DATA KARYAWAN PERUSAHAAN A (TAHUN 2026) ===\n");
+
+        for (Karyawan k : daftarKaryawan) {
+            double gajiSekarang = k.hitungGajiTotal();
             k.displayInfo();
-            System.out.printf("Total Gaji Bulan Ini: Rp%.0f\n", gaji);
-            System.out.println("-------------------------------------------------");
+            System.out.printf("Total Gaji Bulan Ini: Rp%.0f\n", gajiSekarang);
+            System.out.println("-----------------------------------------------");
 
-            totalAnggaran += gaji; 
-            totalRating += k.getRatingKinerja(); 
+            totalPengeluaranGaji += gajiSekarang; 
+            akumulasiRating += k.getRatingKinerja(); 
 
-            if (gaji > highestEarner.hitungGajiTotal()) {
-                highestEarner = k;
+            if (gajiSekarang > karyawanGajiTertinggi.hitungGajiTotal()) {
+                karyawanGajiTertinggi = k;
             }
         }
 
-        System.out.println("\n=== KESIMPULAN DATA PERUSAHAAN ===");
-        System.out.printf("Total Anggaran Gaji: Rp%.0f\n", totalAnggaran);
-        System.out.println("Highest Earner: " + highestEarner.getNama());
-        System.out.printf("Rata-rata Rating Kinerja: %.2f\n", (totalRating / listKaryawan.size()));
+        double rataRataPerforma = akumulasiRating / daftarKaryawan.size();
+
+        System.out.println("\n=== CEK HASIL AKHIR ===");
+        System.out.printf("Total Gaji Semua Karyawan : Rp%.0f\n", totalPengeluaranGaji);
+        System.out.println("Juara Gaji Terbesar       : " + karyawanGajiTertinggi.getNama());
+        System.out.printf("Rata-rata Rating Kerja    : %.2f\n", rataRataPerforma);
     }
 }
